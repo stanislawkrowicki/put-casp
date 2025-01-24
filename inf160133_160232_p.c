8a014eb7a65e3bf90d82358088892bba2ab8793b
Stanisław Krowicki <krowicki00@gmail.com>
2025-01-24 16:01:34 +0100

    Producer login responses

--- inf160133_160232_p.c
+++ inf160133_160232_p.c
@@ -5,8 +5,58 @@
 #include <sys/msg.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "inf160133_160232_types.h"
+
+int G_queue;
+int G_id;
+int G_producing_type;
+
+void login_ok()
+{
+    printf("Login accepted~\n");
+}
+
+void login_failed()
+{
+    printf("Login failed. Producer already exists or there are too many producers.\n");
+}
+
+void wait_for_login_response()
+{
+    system_type ok_id = get_system_type(G_id, DISP2PROD_LOGIN_OK);
+    system_type failed_id = get_system_type(G_id, DISP2PROD_LOGIN_FAILED);
+
+    struct system_message response;
+
+    while (1)
+    {
+        ssize_t msg_size = msgrcv(G_queue, &response, sizeof(response.payload), ok_id, IPC_NOWAIT);
+        if (msg_size != -1)
+        {
+            login_ok();
+            break;
+        }
+        else if (errno != ENOMSG)
+        {
+            perror("msgrcv error while waiting for login response\n");
+        }
+
+        msg_size = msgrcv(G_queue, &response, sizeof(response.payload), failed_id, IPC_NOWAIT);
+        if (msg_size != -1)
+        {
+            login_failed();
+            break;
+        }
+        else if (errno != ENOMSG)
+        {
+            perror("msgrcv error while waiting for login response\n");
+        }
+
+        usleep(100);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,43 +66,41 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int id = atoi(argv[1]);
-    int type = atoi(argv[2]);
+    G_id = atoi(argv[1]);
+    G_producing_type = atoi(argv[2]);
 
-    if (id < 1 || id > MAX_ID)
+    if (G_id < 1 || G_id > MAX_ID)
     {
         printf("ID musi być pomiędzy 1 a %d\n", MAX_ID);
         return 1;
     }
 
-    if (type < 1 || type > MAX_NOTIFICATION)
+    if (G_producing_type < 1 || G_producing_type > MAX_NOTIFICATION)
     {
         printf("Typ wiadomości musi być pomiędzy 1 a %d.", MAX_NOTIFICATION);
         return 1;
     }
 
-    int queue = msgget(P_SYSTEM_QUEUE_ID, 0600 | IPC_CREAT);
+    G_queue = msgget(P_SYSTEM_QUEUE_ID, 0600 | IPC_CREAT);
 
     struct system_message msg;
 
     msg.mtype = get_system_type(DISPATCHER_ID, PROD2DISP_LOGIN);
 
-    // char text[] = "Hello!";
+    msg.payload.numbers[0] = G_id;             // Producer ID
+    msg.payload.numbers[1] = G_producing_type; // Type to listen to
 
-    msg.payload.numbers[0] = id;   // Producer ID
-    msg.payload.numbers[1] = type; // Type to listen to
-
-    printf("Message ID: %ld\n", msg.mtype);
-
-    if (msgsnd(queue, &msg, sizeof(msg.payload), 0) == -1)
+    if (msgsnd(G_queue, &msg, sizeof(msg.payload), 0) == -1)
     {
         perror("msgsnd error");
         exit(1);
     }
     else
     {
-        printf("Message sent!\n");
+        printf("Login request sent!\n");
     }
+
+    wait_for_login_response();
 
     return 0;
 }
